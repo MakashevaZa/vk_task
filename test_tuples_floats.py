3b3bf2e492c62dc5b83ae89232cfc2802ecc4c40
MakashevaZa <zarina.ma.93@yandex.ru>
2022-09-12 23:44:04 +0300

    second commit

---
+++ test_tuples_floats.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+'''
+@pytest.mark.tuples
+@pytest.mark.parametrize('tuples', [(), (), ()])
+def test_tuples(tuples):
+    try:
+
+    except SomeError:
+
+    finally:
+
+@pytest.mark.float
+def test_floats():
+    '''
